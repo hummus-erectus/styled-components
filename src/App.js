399720203ebbe89acf6d754c50888a8be29e7886
hummus-erectus <robert.samuel.grayson@gmail.com>
2023-05-06 15:00:43 +0900

    Completed tutorial

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import { ThemeProvider } from 'styled-components'
 import { Container } from './components/styles/Container.styled'
 import Header from './components/Header'
+import Footer from './components/Footer'
+import Card from './components/Card'
 import GlobalStyles from './components/styles/Global'
+import content from './content'
 
 const theme = {
   colors: {
     header: '#ebfbff',
     body: '#fff',
     footer: '#003333'
-  }
+  },
+  mobile: '768px',
 }
 
 function App() {
@@ -18,11 +22,14 @@ function App() {
         <GlobalStyles />
         <Header />
         <Container>
-          <h1>Hello World</h1>
+          {content.map((item, index) => (
+            <Card key={index} item={item} />
+          ))}
         </Container>
+        <Footer />
       </>
     </ThemeProvider>
   );
 }
 
-export default App;
+export default App
